71d48a11ee32a868b6663b385bc843ff229afb7a
nikosavic <nsavic85@gmail.com>
2026-01-28 14:26:29 +0300

    Update Project fillable for bilingual fields

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,15 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = [
-        'title',
         'slug',
-        'summary',
+    
+        'title',
+        'title_en',
+        'title_tr',
+    
+        'excerpt_en',
+        'excerpt_tr',
+    
         'body',
+        'body_en',
+        'body_tr',
+    
         'repo_url',
         'live_url',
+    
         'featured',
         'sort_order',
-        'status',
+    
+        'is_published',
+        'published_at',
     ];
 
     protected $casts = [
