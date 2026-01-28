b96123b0567c0f73db00c03022cc0854c526958b
nikosavic <nsavic85@gmail.com>
2026-01-28 11:35:27 +0300

    Add Mailgun mailer config

--- config/mail.php
+++ config/mail.php
@@ -49,6 +49,10 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'mailgun' => [
+            'transport' => 'mailgun',
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
@@ -88,6 +92,8 @@ return [
             'retry_after' => 60,
         ],
 
+        
+
         'roundrobin' => [
             'transport' => 'roundrobin',
             'mailers' => [
@@ -114,5 +120,7 @@ return [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
+    'contact_to' => env('MAIL_CONTACT_TO', env('MAIL_FROM_ADDRESS')),
+
 
 ];
